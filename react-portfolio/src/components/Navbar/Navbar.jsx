bb161e4f72c75851025c8be200b3461fcdeccbb1
paulmcgonigle <S00210326@atu.ie>
2025-01-21 14:01:52 -0600

    moved images to assets and set state for nav

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import menuIcon from "../../assets/nav/menuIcon.png"; // Corrected import path
+import menuIcon from "../../assets/nav/menuIcon.png";
+import closeIcon from "../../assets/nav/closeIcon.png";
 import styles from "./Navbar.module.css";
 
 // import { getImageUrl } from "../../utils";
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
         Portfolio
       </a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={menuIcon} alt="menu-button" />
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? menuIcon : closeIcon}
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
         <ul className={styles.menuItems}>
           <li>
             <a href="#about">About</a>
