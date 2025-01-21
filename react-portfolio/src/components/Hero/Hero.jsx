376ec2ed5a31f948b78be8cb29cb755cc91515ee
paulmcgonigle <S00210326@atu.ie>
2025-01-21 16:03:11 -0600

    added some animation for the photos etc

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -3,18 +3,20 @@ import hero from "../../assets/hero/hero.png";
 import styles from "./Hero.module.css";
 function Hero() {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Paul</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Paul</h1>
+        <p className={styles.description}>
           I'm a full stack developerwith over 4 years of practical experience,
           mainly in Full-stack development with framworks like React, but also
           with knowledge of cloud , .NET and much more. Reach out if youd like
           to learn more,
         </p>
-        <a href="mailto:pgonigle57@gmail.com">Contact Me</a>
+        <a className={styles.contactBtn} href="mailto:pgonigle57@gmail.com">
+          Contact Me
+        </a>
       </div>
-      <img src={hero} alt="heroimg"></img>
+      <img src={hero} alt="heroimg" className={styles.heroImg}></img>
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
