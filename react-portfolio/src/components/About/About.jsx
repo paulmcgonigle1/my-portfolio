0cce17537bd393fcb279b59071d52bd010490dc3
paulmcgonigle <S00210326@atu.ie>
2025-01-21 16:27:15 -0600

    finished styling on about

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -7,14 +7,18 @@ import styles from "./About.module.css";
 
 function About() {
   return (
-    <section>
-      <h2>About</h2>
-      <div>
-        <img src={aboutImage} alt="me sitting with laptop" />
-        <ul>
-          <li>
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          className={styles.aboutImage}
+          src={aboutImage}
+          alt="me sitting with laptop"
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
             <img src={cursorIcon} alt="cursor Icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
                 Im a frontend developer with experience in responsive and
@@ -22,18 +26,18 @@ function About() {
               </p>
             </div>
           </li>
-          <li>
+          <li className={styles.aboutItem}>
             <img src={serverIcon} alt="server Icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
                 I have expereince developing fast and optimised backend systems
               </p>
             </div>
           </li>
-          <li>
+          <li className={styles.aboutItem}>
             <img src={uiIcon} alt="UI Icon" />
-            <div>
+            <div className={styles.aboutItemText}>
               <h3>.NET Developer</h3>
               <p>
                 I have loads of expertise in working with .NET, ASP.NET CORE,
